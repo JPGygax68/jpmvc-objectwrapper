8e12bdd847f5968aea8721b4cf369038bbab554d
Hans-Peter Gygax <gygax@practicomp.ch>
2013-09-28 19:45:53 +0200

    get(), set() methods

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ function Model(object) {
   this.object = object;
 }
 
+Model.prototype.get = function(key) {
+  return this.object[key];
+}
+
+Model.prototype.set = function(key, value) {
+  return (this.object[key] = value);
+}
+
 function wrap(object) {
 
   var wrapper = _.findWhere(cache, { object: object });
--- test.js
+++ test.js
@@ -5,3 +5,11 @@ var should = require('should');
 var wrapper = require('./main');
 
 ( !! wrapper.wrap({}) ).should.be.ok;
+
+var obj1 = { firstname: 'Jean-Pierre', lastname: 'Gygax' };
+
+var model = wrapper.wrap(obj1);
+
+model.get('firstname').should.be.equal('Jean-Pierre');
+
+model.set('firstname', 'Hans-Peter').should.be.equal('Hans-Peter');
